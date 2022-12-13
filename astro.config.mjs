48d24538e1d998c05ce2ddad90562723de69b013
fiqryq <fiqrychoerudin48@gmail.com>
2022-12-14 00:03:45 +0700

    feat: integrate with react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
